2c752e3092963456a9b798bc0effea5eab837521
Li Li <by1908007@buaa.edu.cn>
2020-02-26 16:27:12 +0800

    change the range of t

--- R/gen_train_data.R
+++ R/gen_train_data.R
@@ -11,8 +11,8 @@ library(tseries)
 
 ## library(M4metaresults)
 
-## library(M4comp2018)
-load("data/M4.rda")
+library(M4comp2018)
+#load("data/M4.rda")
 
 ##data(m4 quarterly data)
 M4_q<-M4[23001:47000]
@@ -36,8 +36,7 @@ model_conf = list(
   , forecast_h = 8 # Forecasting horizon
   , train_h = 1 # Out of sample training, 1 means rolling forecast
   , history_burn = 16 # Let the model to start with at least this length of historical data.
-  , PI_level = 90 # Predictive Interval level, used to extract out-of-sample variance from
-              # forecasting models.
+  , PI_level = 90 # Predictive Interval level, used to extract out-of-sample variance from forecasting models.
   , intercept = FALSE # Should intercept in the features
 )
 ###----------------------------------------------------------------------------
@@ -57,15 +56,18 @@ for (i_ts in 1:length(data))
 
   y01 = as.numeric(y01)
 
+  #****log_pred_densities nrow+1 ****
   ## Calculate historical log predictive density
-  log_pred_densities<-matrix(nrow = length(y) - history_burn - train_h, ncol = 2)
-  for (t in (history_burn + 1):(length(y) - train_h))
+  log_pred_densities<-matrix(nrow = length(y) - history_burn - train_h +1, ncol = 2)
+  
+  #**** changed the value of t ****
+  for (t in (history_burn):(length(y) - train_h))
   {
     ## TODO: We may simplify this to assume the fit and forecast procedure is
     ## invariant with certain time period to save time.
 
     ## ETS model
-    ets_fit <- ets(y01[1:(t-train_h)], model = ets_model)
+    ets_fit <- ets(y01[1:t], model = ets_model)
     ets_fore<-forecast(ets_fit, h = train_h, level = PI_level)
     ets_fore_mean <- ets_fore$mean
     ## forecast.ets does not directly provide predictive variance but we could infer from
@@ -73,27 +75,30 @@ for (i_ts in 1:length(data))
     ets_fore_sd = (ets_fore$lower - ets_fore$mean)/qnorm(1 - PI_level/100)
 
     ## ARIMA model
-    arima_fit <- auto.arima(y01[1:(t - train_h)])
+    arima_fit <- auto.arima(y01[1:t])
     arima_fore <- forecast(arima_fit, h = train_h, level = PI_level)
     arima_fore_mean <- arima_fore$mean
     arima_fore_sd = (arima_fore$lower - arima_fore$mean)/qnorm(1 - PI_level/100)
 
     ## To keep numeric stability, we calculate log P(y_pred)
-    log_pred_densities[(t - history_burn), 1] <- sum(dnorm(y01[(t + 1):(t + train_h)], mean = ets_fore_mean,
+    log_pred_densities[(t - history_burn +1), 1] <- sum(dnorm(y01[(t + 1):(t + train_h)], mean = ets_fore_mean,
                                                            sd = ets_fore_sd, log = TRUE))
-    log_pred_densities[(t - history_burn), 2] <- sum(dnorm(y01[(t + 1):(t + train_h)], mean = arima_fore_mean,
+    log_pred_densities[(t - history_burn +1), 2] <- sum(dnorm(y01[(t + 1):(t + train_h)], mean = arima_fore_mean,
                                                            sd = arima_fore_sd, log = TRUE))
   }
   lpred_dens[[i_ts]]<-log_pred_densities
-
+  
+  #**** features_y nrow+1 ****
   ## Calculate historical features
-  features_y <- matrix(nrow = length(y) - train_h - history_burn, ncol = 42)
-  for (t in (history_burn + 1):(length(y) - train_h))
+  features_y <- matrix(nrow = length(y) - train_h - history_burn +1, ncol = 42)
+  
+  #**** changed the value of t ****
+  for (t in (history_burn):(length(y) - train_h))
   {
-    myts <-list(list(x=ts(y[1:(t - train_h)], frequency = frequency)))
+    myts <-list(list(x=ts(y[1:t], frequency = frequency)))
     myfeatures <- THA_features(myts)[[1]]$features
     myfeatures <- data.matrix(myfeatures)
-    features_y[(t - history_burn), ] <- myfeatures
+    features_y[(t - history_burn +1), ] <- myfeatures
   }
 
   features_y_scaled = scale(features_y, center = TRUE, scale = TRUE)
@@ -108,4 +113,4 @@ for (i_ts in 1:length(data))
   ## }
 
 }
-save(feat, lpred_dens, data, model_conf, file="data/historical_log_pred_features.RData")
+save(feat, lpred_dens, data, model_conf, file="E:/time series/R code/feature-based-bayesian-model-averaging/data/historical_log_pred_features.RData")
