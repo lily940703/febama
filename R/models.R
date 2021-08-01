f0f601cdc102e15ab7981de1fe42abb9672f0615
Li-Li <54852360+lily940703@users.noreply.github.com>
2021-08-01 20:09:29 +0800

    Update models.R

--- R/models.R
+++ R/models.R
@@ -1,14 +1,14 @@
-#' @references FFORMA: Feature-based Forecast Model Averaging
-#' @models in 'forecast': ets, auto.arima, naive, rw_drift, snaive, stlm_ar, thetaf
-#' @details: Obtain the forecasting sd by assuming a Gaussian interval
-#' @param x: A \code{ts} object with the input time series
-#' @param train_h: The amount of future time steps to forecast
-#' @param PI_level: Predictive Interval level, used to extract out-of-sample variance from forecasting models.
-#' @return A list including forecasting mean and sd
+#' The forecasting model ETS basd on \code{forecast::ets}
+#'
+#' @param x The input time series.
+#' @param train_h The amount of future time steps to forecast.
+#' @param PI_level Predictive Interval level, used to extract out-of-sample variance from forecasting models.
+#'
+#' @return A list including forecasting mean and sd.
 #' @export
 
 ets_fore <- function(x, train_h, PI_level) {
-  ets_fit <- forecast::ets(x, model = "ANN")
+  ets_fit <- forecast::ets(x, model = "AAN")
   ets_fore <- forecast::forecast(ets_fit, h = train_h, level = PI_level)
   ets_fore_mean <- ets_fore$mean
   ets_fore_sd <- (ets_fore$lower - ets_fore$mean) / qnorm(1 - PI_level / 100)
@@ -16,6 +16,14 @@ ets_fore <- function(x, train_h, PI_level) {
               ets_fore_sd = as.numeric(ets_fore_sd) ))
 }
 
+#' The forecasting model ARIMA basd on \code{forecast::auto.arima}
+#'
+#' @param x The input time series.
+#' @param train_h The amount of future time steps to forecast.
+#' @param PI_level Predictive Interval level, used to extract out-of-sample variance from forecasting models.
+#'
+#' @return A list including forecasting mean and sd.
+#' @export
 auto.arima_fore <- function(x, train_h, PI_level) {
   arima_fit <- forecast::auto.arima(x)
   arima_fore <- forecast::forecast(arima_fit, h = train_h, level = PI_level)
@@ -25,7 +33,14 @@ auto.arima_fore <- function(x, train_h, PI_level) {
               arima_fore_sd = as.numeric(arima_fore_sd) ))
 }
 
-
+#' The forecasting model naive basd on \code{forecast::naive}
+#'
+#' @param x The input time series.
+#' @param train_h The amount of future time steps to forecast.
+#' @param PI_level Predictive Interval level, used to extract out-of-sample variance from forecasting models.
+#'
+#' @return A list including forecasting mean and sd.
+#' @export
 naive_fore <- function(x, train_h, PI_level) {
   naive_fit <- forecast::naive(x, h = train_h, level = PI_level)
   naive_fore_mean <- naive_fit$mean
@@ -34,6 +49,14 @@ naive_fore <- function(x, train_h, PI_level) {
               naive_fore_sd = as.numeric(naive_fore_sd) ))
 }
 
+#' The forecasting model random walk basd on \code{forecast::rwf}
+#'
+#' @param x The input time series.
+#' @param train_h The amount of future time steps to forecast.
+#' @param PI_level Predictive Interval level, used to extract out-of-sample variance from forecasting models.
+#'
+#' @return A list including forecasting mean and sd.
+#' @export
 rw_drift_fore <- function(x, train_h, PI_level) {
   rw_drift_fit <- forecast::rwf(x, drift=TRUE, h = train_h, level = PI_level)
   rw_drift_fore_mean <- rw_drift_fit$mean
@@ -42,100 +65,73 @@ rw_drift_fore <- function(x, train_h, PI_level) {
               rw_drift_fore_sd = as.numeric(rw_drift_fore_sd) ))
 }
 
-snaive_fore <- function(x, train_h, PI_level) {
-  snaive_fit <- forecast::snaive(x, h = train_h, level = PI_level)
-  snaive_fore_mean <- snaive_fit$mean
-  snaive_fore_sd <- (snaive_fit$lower - snaive_fit$mean) / qnorm(1 - PI_level /100)
-  return(list(snaive_fore_mean = as.numeric(snaive_fore_mean),
-              snaive_fore_sd = as.numeric(snaive_fore_sd) ))
-}
 
-stlm_ar_fore <- function(x, train_h, PI_level) {
-  stlm_ar_fit <- tryCatch({
-    forecast::stlm(x, modelfunction = stats::ar)
-  }, error = function(e) forecast::auto.arima(x, d=0,D=0))
-  stlm_ar_fore <- forecast::forecast(stlm_ar_fit, h = train_h, level = PI_level)
-  stlm_ar_fore_mean <- stlm_ar_fore$mean
-  stlm_ar_fore_sd <- (stlm_ar_fore$lower - stlm_ar_fore$mean) / qnorm(1 - PI_level /100)
-  return(list(stlm_ar_fore_mean = as.numeric(stlm_ar_fore_mean),
-              stlm_ar_fore_sd = as.numeric(stlm_ar_fore_sd) ))
-}
 
-thetaf_fore <- function(x, train_h, PI_level) {
-  thetaf_fit <- forecast::thetaf(x, h = train_h, level = PI_level)
-  thetaf_fore_mean <- thetaf_fit$mean
-  thetaf_fore_sd <- (thetaf_fit$lower - thetaf_fit$mean) / qnorm(1 - PI_level /100)
-  return(list(thetaf_fore_mean = as.numeric(thetaf_fore_mean),
-              thetaf_fore_sd = as.numeric(thetaf_fore_sd) ))
-}
 
-#' @models in 'rugarch': garch, egarch, tgarch
-#' @details: parameters p = q = 1
-#' @param x: A \code{ts} object with the input time series
-#' @param train_h: The amount of future time steps to forecast
-#' @param PI_level: The following functions don't use it in the calculation.
-#  This is used as an input parameter to facilitate calculation with the models above.
-#' @return A list including forecasting mean and sd                          
-#' @export   
-                          
+#' The forecasting model garch basd on \code{rugarch} package
+#'
+#' @param x The input time series.
+#' @param train_h The amount of future time steps to forecast.
+#' @param PI_level This function don't use it in the calculation.
+#' This is used to make this function consistent with the parameter setting of the top four models.
+#'
+#' @return A list including forecasting mean and sd.
+#' @export
 garch_fore <- function(x, train_h,  PI_level) {
-  myspec=ugarchspec()
-  myfit = ugarchfit(data=x, spec = myspec, solver="hybrid")
-  fore = ugarchforecast(myfit, n.ahead=train_h)
+  myspec = rugarch::ugarchspec()
+  options(warn =-1)
+  myfit = rugarch::ugarchfit(data=x, spec = myspec, solver="hybrid")
+  fore = rugarch::ugarchforecast(myfit, n.ahead=train_h)
+  rm(myfit)
   garch_fore_mean <- fitted(fore)
   garch_fore_sd <- sigma(fore)
+  rm(fore)
   return(list(garch_fore_mean = as.numeric(garch_fore_mean),
               garch_fore_sd = as.numeric(garch_fore_sd) ))
 }
 
+#' The forecasting model egarch basd on \code{rugarch} package
+#'
+#' @param x The input time series.
+#' @param train_h The amount of future time steps to forecast.
+#' @param PI_level This function don't use it in the calculation.
+#' This is used to make this function consistent with the parameter setting of the top four models.
+#'
+#' @return A list including forecasting mean and sd.
+#' @export
 egarch_fore <- function(x, train_h, PI_level) {
-  myspec=ugarchspec(
-    variance.model = list(model = "eGARCH", garchOrder = c(1, 1), submodel = NULL, 
+  myspec_e = rugarch::ugarchspec(
+    variance.model = list(model = "eGARCH", garchOrder = c(1, 1), submodel = NULL,
                           external.regressors = NULL, variance.targeting = FALSE),
-    mean.model = list(armaOrder = c(1, 1), include.mean = TRUE,
+    mean.model = list(armaOrder = c(0, 0), include.mean = TRUE,
                       archm = FALSE, archpow = 1, arfima = FALSE,
                       external.regressors = NULL, archex = FALSE),
     distribution.model = "norm"
   )
-  
-  myfit = ugarchfit(myspec, data=x, solver="hybrid")
-  fore = ugarchforecast(myfit, n.ahead=train_h)
+  options(warn =-1)
+  myfit_e = rugarch::ugarchfit(myspec_e, data=x, solver="hybrid")
+  fore = rugarch::ugarchforecast(myfit_e, n.ahead=train_h)
+  rm(myfit_e)
   egarch_fore_mean <- fitted(fore)
   egarch_fore_sd <- sigma(fore)
+  rm(fore)
   return(list(egarch_fore_mean = as.numeric(egarch_fore_mean),
               egarch_fore_sd = as.numeric(egarch_fore_sd) ))
 }
 
 
-# tgarch_fore <- function(x, train_h, PI_level) {
-#   myspec=ugarchspec(
-#     variance.model = list(model = "fGARCH", garchOrder = c(1, 1), submodel = "TGARCH", 
-#                           external.regressors = NULL, variance.targeting = FALSE),
-#     mean.model = list(armaOrder = c(1, 1), include.mean = TRUE,
-#                       archm = FALSE, archpow = 1, arfima = FALSE,
-#                       external.regressors = NULL, archex = FALSE),
-#     distribution.model = "norm"
-#   )
-#   
-#   myfit = ugarchfit(myspec, data=x, solver="hybrid")
-#   fore = ugarchforecast(myfit, n.ahead=train_h)
-#   tgarch_fore_mean <- fitted(fore)
-#   tgarch_fore_sd <- sigma(fore)
-#   return(list(tgarch_fore_mean = as.numeric(tgarch_fore_mean),
-#               tgarch_fore_sd = as.numeric(tgarch_fore_sd) ))
-# }
-
-#' @models in 'stochvol': sv
-#' @details: default parameters 
-#' @param x: A \code{ts} object with the input time series
-#' @param train_h: The amount of future time steps to forecast
-#' @param PI_level: The following functions don't use it in the calculation.
-#  This is used as an input parameter to facilitate calculation with the models above.
-#' @return A list including forecasting mean and sd                          
-#' @export   
+#' The forecasting model garch basd on \code{stochvol} package
+#'
+#' @param x The input time series.
+#' @param train_h The amount of future time steps to forecast.
+#' @param PI_level This function don't use it in the calculation.
+#' This is used to make this function consistent with the parameter setting of the top four models.
+#'
+#' @return A list including forecasting mean and sd.
+#' @export
 sv_fore <- function(x, train_h,  PI_level) {
   options(warn =-1)
-  myfit = svsample(y = x, quiet	= T)
+  myfit = stochvol::svsample(y = x, quiet	= T)
   fore = predict(myfit, train_h)
   sv_fore_mean <- mean(fore$y)
   sv_fore_sd <- sqrt(mean(exp(fore$h)))
